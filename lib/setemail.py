028608459b0e851431a84061a2f3012b3b748335
Drew Synan <dsynan@gmail.com>
2017-08-18 12:35:39 -0500

    yikes

--- lib/setemail.py
+++ lib/setemail.py
@@ -14,7 +14,7 @@ def main():
 
 	if len(sys.argv) < 2:
 		# no email specified, prompt for one
-		email = prompt("Veeva notification email? ").strip("\r").strip("\n")
+		email = input("Veeva notification email? ").strip("\r").strip("\n")
 		if email == '':
 			raise RuntimeError("Please specify an email address")
 	else:
